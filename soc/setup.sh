7092e2e833e754daac952d14757b49cd0c37a68d
RoSE Authors <roseauthors@berkeley.edu>
2023-03-15 20:02:52 -0700

    add default local files

--- soc/setup.sh
+++ soc/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-PROJECT_ROOT='/home/centos'
+PROJECT_ROOT='/scratch/vnikiforov'
 FIRESIM_DIR=${PROJECT_ROOT}/firesim
 ROSE_DIR=${PROJECT_ROOT}/RoSE
 
-cp ${ROSE_DIR}/soc/sim/config_runtime.yaml ${FIRESIM_DIR}/deploy
+cp ${ROSE_DIR}/soc/sim/config_runtime_local.yaml ${FIRESIM_DIR}/deploy/config_runtime.yaml
+cp ${ROSE_DIR}/soc/sim/config_build_recipes_local.yaml ${FIRESIM_DIR}/deploy/config_build_recipes.yaml
+cp ${ROSE_DIR}/soc/sim/config_build_local.yaml ${FIRESIM_DIR}/deploy/config_build.yaml
