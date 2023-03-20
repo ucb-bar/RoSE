d620d16d63260fa8a0f6aff6a434f59394cdee31
RoSE Authors <roseauthors@berkeley.edu>
2023-03-19 22:21:52 -0700

    dnn based workload config files and models

---
+++ soc/sw/rose-images/airsim-driver-fed/host-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will run on the host from the workload directory
+# (e.g. workloads/example-fed) every time the workload is built.
+# It is recommended to call into something like a makefile because
+# this script may be called multiple times.
