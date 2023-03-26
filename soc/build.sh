a31b9411c88f573787e8a7c1ce7d7b7cfdbc9773
RoSE Authors <roseauthors@berkeley.edu>
2023-03-26 07:36:19 -0700

    update build script to use absolute directory

--- soc/build.sh
+++ soc/build.sh
@@ -4,6 +4,7 @@ FIRESIM_DIR=${PROJECT_ROOT}/firesim
 CHIPYARD_DIR=${FIRESIM_DIR}/target-design/chipyard
 ROSE_DIR=${PROJECT_ROOT}/RoSE
 
+cd ${ROSE_DIR}/soc/
 cd ./sw/onnxruntime-riscv/
 ./build.sh --enable_training --config=Debug --cmake_extra_defines onnxruntime_USE_SYSTOLIC=ON onnxruntime_SYSTOLIC_INT8=OFF onnxruntime_SYSTOLIC_FP32=ON
 cd ./systolic_runner/imagenet_runner/ && bash ./build.sh  --enable_training && cd ../..
