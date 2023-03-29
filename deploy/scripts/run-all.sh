9b47f6ff6dcf40fe3d371ac2c4e1c93ed7581f92
RoSE Authors <roseauthors@berkeley.edu>
2023-03-29 12:58:48 -0700

    update global running script

--- deploy/scripts/run-all.sh
+++ deploy/scripts/run-all.sh
@@ -7,6 +7,6 @@ cd ${ROSE_DIR}
 bash ${ROSE_DIR}/deploy/scripts/tunnel-exp.sh
 bash ${ROSE_DIR}/deploy/scripts/rose-hw-sw-sweep.sh
 bash ${ROSE_DIR}/deploy/scripts/rose-velocity-sweep.sh
-bash ${ROSE_DIR}/deploy/scripts/rose-dynamic_exp.sh
+bash ${ROSE_DIR}/deploy/scripts/rose-dynamic-exp.sh
 bash ${ROSE_DIR}/deploy/scripts/rose-perf-sync-only.sh
 bash ${ROSE_DIR}/deploy/scripts/rose-perf-tunnel-exp.sh
