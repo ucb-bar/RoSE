9c4d2625aac5fa9290ef8cdb874535181fd1cd13
Lux Zhang <iansseijelly@firesim1.Millennium.Berkeley.EDU>
2023-04-07 17:16:47 -0700

    updated soc/setup.sh to be scalable and use symlinks

--- soc/setup.sh
+++ soc/setup.sh
@@ -6,32 +6,93 @@ ROSE_DIR=${PROJECT_ROOT}/RoSE
 SCALA_DIR=${ROSE_DIR}/soc/src/main/scala
 FSIM_CC_DIR=${ROSE_DIR}/soc/src/main/cc
 
-# Copy scala files
-cp ${SCALA_DIR}/AirSimIO.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/example/
-cp ${SCALA_DIR}/IOBinders.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/
-cp ${SCALA_DIR}/BridgeBinders.scala ${CHIPYARD_DIR}/generators/firechip/src/main/scala/
-cp ${SCALA_DIR}/DigitalTop.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/
-cp ${SCALA_DIR}/AbstractConfig.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/
-cp ${SCALA_DIR}/AirSimBridge.scala  ${FIRESIM_DIR}/sim/firesim-lib/src/main/scala/bridges/
-cp ${SCALA_DIR}/RoSEConfigs.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/
+# Create an array of source files
+sources=(
+    #scala files
+    "${SCALA_DIR}/AirSimIO.scala"
+    "${SCALA_DIR}/IOBinders.scala" 
+    "${SCALA_DIR}/BridgeBinders.scala" 
+    "${SCALA_DIR}/DigitalTop.scala" 
+    "${SCALA_DIR}/AbstractConfig.scala" 
+    "${SCALA_DIR}/AirSimBridge.scala" 
+    "${SCALA_DIR}/RoSEConfigs.scala"
+    #C++ files
+    "${FSIM_CC_DIR}/airsim.cc"
+    "${FSIM_CC_DIR}/airsim.h"
+    "${FSIM_CC_DIR}/firesim_top.cc"
+    #simulation configs
+    "${ROSE_DIR}/soc/sim/config_runtime_local.yaml"
+    "${ROSE_DIR}/soc/sim/config_build_recipes_local.yaml"
+    "${ROSE_DIR}/soc/sim/config_build_local.yaml"
+    "${ROSE_DIR}/soc/sim/config_hwdb_local.yaml"
+    #workload configs
+    "${ROSE_DIR}/soc/sim/airsim-driver-fed.json"
+    "${ROSE_DIR}/soc/sim/airsim-control-fed.json"
+)
 
-# Copy C++ files
-cp ${FSIM_CC_DIR}/airsim.cc ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
-# cp ${FSIM_CC_DIR}/heartbeat.cc ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
-cp ${FSIM_CC_DIR}/airsim.h ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
-# cp ${FSIM_CC_DIR}/heartbeat.h ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
-cp ${FSIM_CC_DIR}/firesim_top.cc ${FIRESIM_DIR}/sim/src/main/cc/firesim/
+# Create an array of destination files
+destinations=(
+    #scala destinations
+    "${CHIPYARD_DIR}/generators/chipyard/src/main/scala/example/AirSimIO.scala"
+    "${CHIPYARD_DIR}/generators/chipyard/src/main/scala/IOBinders.scala"
+    "${CHIPYARD_DIR}/generators/firechip/src/main/scala/BridgeBinders.scala"
+    "${CHIPYARD_DIR}/generators/chipyard/src/main/scala/DigitalTop.scala"
+    "${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/AbstractConfig.scala"
+    "${FIRESIM_DIR}/sim/firesim-lib/src/main/scala/bridges/AirSimBridge.scala"
+    "${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/RoSEConfigs.scala"
+    #C++ destinations
+    "${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/airsim.cc"
+    "${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/airsim.h"
+    "${FIRESIM_DIR}/sim/src/main/cc/firesim/firesim_top.cc"
+    #simulation configs destinations
+    "${FIRESIM_DIR}/deploy/config_runtime.yaml"
+    "${FIRESIM_DIR}/deploy/config_build_recipes.yaml"
+    "${FIRESIM_DIR}/deploy/config_build.yaml"
+    "${FIRESIM_DIR}/deploy/config_hwdb.yaml"
+    #workload configs destinations
+    "${FIRESIM_DIR}/deploy/workloads/airsim-driver-fed.json"
+    "${FIRESIM_DIR}/deploy/workloads/airsim-control-fed.json"
+) 
+
+# Iterate over the arrays and create symbolic links
+for ((i=0;i<${#sources[@]};++i)); do
+    # Check if the destination file or symlink already exists
+    if [[ -e "${destinations[$i]}" || -L "${destinations[$i]}" ]]; then
+        echo "Removing existing file or symbolic link at ${destinations[$i]}..."
+        rm -rf "${destinations[$i]}"
+    fi
+
+    # Create the symbolic link
+    echo "Creating symbolic link from ${sources[$i]} to ${destinations[$i]}..."
+    ln -s "${sources[$i]}" "${destinations[$i]}"
+done
+
+# # Copy scala files
+# cp ${SCALA_DIR}/AirSimIO.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/example/
+# cp ${SCALA_DIR}/IOBinders.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/
+# cp ${SCALA_DIR}/BridgeBinders.scala ${CHIPYARD_DIR}/generators/firechip/src/main/scala/
+# cp ${SCALA_DIR}/DigitalTop.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/
+# cp ${SCALA_DIR}/AbstractConfig.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/
+# cp ${SCALA_DIR}/AirSimBridge.scala  ${FIRESIM_DIR}/sim/firesim-lib/src/main/scala/bridges/
+# cp ${SCALA_DIR}/RoSEConfigs.scala ${CHIPYARD_DIR}/generators/chipyard/src/main/scala/config/
+
+# # Copy C++ files
+# cp ${FSIM_CC_DIR}/airsim.cc ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
+# # cp ${FSIM_CC_DIR}/heartbeat.cc ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
+# cp ${FSIM_CC_DIR}/airsim.h ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
+# # cp ${FSIM_CC_DIR}/heartbeat.h ${FIRESIM_DIR}/sim/firesim-lib/src/main/cc/bridges/
+# cp ${FSIM_CC_DIR}/firesim_top.cc ${FIRESIM_DIR}/sim/src/main/cc/firesim/
 
 
-# Copy simulation configs
-cp ${ROSE_DIR}/soc/sim/config_runtime_local.yaml ${FIRESIM_DIR}/deploy/config_runtime.yaml
-cp ${ROSE_DIR}/soc/sim/config_build_recipes_local.yaml ${FIRESIM_DIR}/deploy/config_build_recipes.yaml
-cp ${ROSE_DIR}/soc/sim/config_build_local.yaml ${FIRESIM_DIR}/deploy/config_build.yaml
-cp ${ROSE_DIR}/soc/sim/config_hwdb_local.yaml ${FIRESIM_DIR}/deploy/config_hwdb.yaml
+# # Copy simulation configs
+# cp ${ROSE_DIR}/soc/sim/config_runtime_local.yaml        ${FIRESIM_DIR}/deploy/config_runtime.yaml
+# cp ${ROSE_DIR}/soc/sim/config_build_recipes_local.yaml  ${FIRESIM_DIR}/deploy/config_build_recipes.yaml
+# cp ${ROSE_DIR}/soc/sim/config_build_local.yaml          ${FIRESIM_DIR}/deploy/config_build.yaml
+# cp ${ROSE_DIR}/soc/sim/config_hwdb_local.yaml           ${FIRESIM_DIR}/deploy/config_hwdb.yaml
 
 # Copy workload configs
-cp ${ROSE_DIR}/soc/sim/airsim-driver-fed.json ${FIRESIM_DIR}/deploy/workloads/
-cp ${ROSE_DIR}/soc/sim/airsim-control-fed.json ${FIRESIM_DIR}/deploy/workloads/
+# cp ${ROSE_DIR}/soc/sim/airsim-driver-fed.json ${FIRESIM_DIR}/deploy/workloads/
+# cp ${ROSE_DIR}/soc/sim/airsim-control-fed.json ${FIRESIM_DIR}/deploy/workloads/
 
 # Init firemarshal submodules
 cd ${FIRESIM_DIR}/sw/firesim-software/
