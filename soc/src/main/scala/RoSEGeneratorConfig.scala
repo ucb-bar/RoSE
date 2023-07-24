132c88b4069533f7a8ebe090bb668d4592f0f88a
Lux Zhang <iansseijelly@firesim1.Millennium.Berkeley.EDU>
2023-07-23 18:14:48 -0700

    updates that fully emits verilog

--- soc/src/main/scala/RoSEGeneratorConfig.scala
+++ soc/src/main/scala/RoSEGeneratorConfig.scala
@@ -13,6 +13,9 @@ import freechips.rocketchip.config.{Parameters, Field, Config}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 
+import firrtl.annotations.{HasSerializationHints}
+
+
 case object RoseAdapterKey extends Field[Option[RoseAdapterParams]](None)
 
 class WithRoseAdapter() extends Config((site, here, up) => {
@@ -26,19 +29,25 @@ case class RoseAdapterParams(
   // This is the width of the queues
   width: Int = 32,
   // Sequence of Destination ports
-  dst_ports: Seq[DstParams] = Seq(
-    DstParams(port_type = "reqrsp", IDs = Seq(0x00), latency = 0, bandwidth = 32),
-    DstParams(port_type = "DMA", IDs = Seq(0x01), DMA_address = 0x88000000L, latency = 0, bandwidth = 32)
-  )
+  dst_ports: DstParams_Container = DstParams_Container(Seq(
+    DstParams(port_type = "DMA", IDs = Seq(1), DMA_address = 0x88000000L, latency = 0, bandwidth = 32),
+    DstParams(port_type = "reqrsp", IDs = Seq(0), latency = 0, bandwidth = 32)
+  ))
   // require none of the dst params ID overlap
   // require(RoseAdapterParams().dst_ports.map(_.IDs).flatten.distinct.size == RoseAdapterParams().dst_ports.map(_.IDs).flatten.size)
   // require less than 30 dst ports
   // require(RoseAdapterParams().dst_ports.size < 30)
-)
+) extends HasSerializationHints {
+  def typeHints: Seq[Class[_]] = Seq(classOf[DstParams_Container])
+}
+
+case class DstParams_Container (seq: Seq[DstParams]) extends HasSerializationHints {
+  def typeHints: Seq[Class[_]] = Seq(classOf[DstParams])
+}
 
 case class DstParams (
   val port_type: String = "reqrsp", // supported are stream, decoupled, interrupt, and DMA
-  val IDs: Seq[Byte] = Seq(0x00), // sequence of ID bytes, must be non-overlapping
+  val IDs: Seq[Int] = Seq(0), // sequence of ID bytes, must be non-overlapping
   val DMA_address: BigInt = 0x88000000L, // this is only used if port_type is DMA
   val latency: Int = 0,
   val bandwidth: Int = 32,
