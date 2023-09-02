cfcc0cc47f16a72844c72b8cde3bdb5cf5e803cf
ROsE USer <rose@garden.CS.Berkeley.EDU>
2023-09-01 17:51:50 -0700

    all garden changes

--- soc/build.sh
+++ soc/build.sh
@@ -9,12 +9,12 @@ ROSE_DIR=${PROJECT_ROOT}/RoSE
 
 cd ${ROSE_DIR}/soc/
 cd ./sw/onnxruntime-riscv/
-./build.sh --enable_training --config=Debug --cmake_extra_defines onnxruntime_USE_SYSTOLIC=ON onnxruntime_SYSTOLIC_INT8=OFF onnxruntime_SYSTOLIC_FP32=ON
+./build.sh --enable_training --parallel --config=Debug --cmake_extra_defines onnxruntime_USE_SYSTOLIC=ON onnxruntime_SYSTOLIC_INT8=OFF onnxruntime_SYSTOLIC_FP32=ON
 cd ./systolic_runner/imagenet_runner/ && bash ./build.sh  --enable_training && cd ../..
 cd ../
 marshal build ${ROSE_DIR}/soc/sw/rose-images/airsim-driver-fed.json
 marshal build ${ROSE_DIR}/soc/sw/rose-images/airsim-control-fed.json
-marshal build ${ROSE_DIR}/soc/sw/rose-images/airsim-packettest.json
+# marshal build ${ROSE_DIR}/soc/sw/rose-images/airsim-packettest.json
 if [ ! -d ${FIRESIM_DIR}/deploy/workloads/airsim-driver-fed ]; then
     mkdir -p ${FIRESIM_DIR}/deploy/workloads/airsim-driver-fed
 fi
@@ -23,15 +23,11 @@ if [ ! -d ${FIRESIM_DIR}/deploy/workloads/airsim-control-fed ]; then
     mkdir -p ${FIRESIM_DIR}/deploy/workloads/airsim-control-fed
 fi
 
-if [ ! -d ${FIRESIM_DIR}/deploy/workloads/airsim-control-fed ]; then
-    mkdir -p ${FIRESIM_DIR}/deploy/workloads/airsim-
-fi
-
 ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-driver-fed-bin  ${FIRESIM_DIR}/deploy/workloads/airsim-driver-fed/airsim-driver-fed-bin
 ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-driver-fed.img  ${FIRESIM_DIR}/deploy/workloads/airsim-driver-fed/airsim-driver-fed.img
 
 ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-control-fed-bin  ${FIRESIM_DIR}/deploy/workloads/airsim-control-fed/airsim-control-fed-bin
 ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-control-fed.img  ${FIRESIM_DIR}/deploy/workloads/airsim-control-fed/airsim-control-fed.img
-
-ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-packettest-bin  ${FIRESIM_DIR}/deploy/workloads/airsim-packettest/airsim-packettest-bin
-ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-packettest.img  ${FIRESIM_DIR}/deploy/workloads/airsim-packettest/airsim-packettest.img
+# 
+# ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-packettest-bin  ${FIRESIM_DIR}/deploy/workloads/airsim-packettest/airsim-packettest-bin
+# ln -s ${FIRESIM_DIR}/sw/firesim-software/images/airsim-packettest.img  ${FIRESIM_DIR}/deploy/workloads/airsim-packettest/airsim-packettest.img
