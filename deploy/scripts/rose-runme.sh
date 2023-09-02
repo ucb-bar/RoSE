cfc93c091a312ed86599248f1c11037884825116
ROsE USer <rose@garden.CS.Berkeley.EDU>
2023-09-01 17:51:50 -0700

    all garden changes

--- deploy/scripts/rose-runme.sh
+++ deploy/scripts/rose-runme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_ROOT="/scratch/$(whoami)"/rose-parent-dir
+PROJECT_ROOT="/scratch"
 ROSE_DIR=${PROJECT_ROOT}/RoSE
 
 BOOM_CONFIG='firesim-dual-large-boom-fp32gemmini-singlecore-with-airsim-no-nic-l2-llc4mb-ddr3'
@@ -10,6 +10,7 @@ START_Y='0.0'
 END_X='-80.0'
 END_CYCLE=40_000_000_000
 AIRSIM_STEPS=1
+#FIRESIM_CYCLES=10_000_000
 FIRESIM_CYCLES=10_000_000
 ANGLE=200.0
 DNN='trail_resnet14_complex.onnx'
@@ -18,5 +19,5 @@ VEL=9
 
 cd ${ROSE_DIR}/deploy/hephaestus
 
-python3 runner.py -r FireSim -a ${AIRSIM_STEPS} -f ${FIRESIM_CYCLES} -y ${START_Y} -c ${END_CYCLE} -x ${END_X} -l ${ROSE_DIR}/deploy/hephaestus/logs/lux-debug # | tee ${ROSE_DIR}/deploy/hephaestus/logs/tunnel-exp-rocket-gemmini-${ANGLE_NAMES[$i]}.log
+python3 runner.py -i localhost -r FireSim -a ${AIRSIM_STEPS} -f ${FIRESIM_CYCLES} -y ${START_Y} -c ${END_CYCLE} -x ${END_X} -l ${ROSE_DIR}/deploy/hephaestus/logs/lux-debug # | tee ${ROSE_DIR}/deploy/hephaestus/logs/tunnel-exp-rocket-gemmini-${ANGLE_NAMES[$i]}.log
 firesim kill &
