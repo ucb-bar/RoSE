f993cb73817a3568a898bd9a50f03bda1bd16f97
Lux <iansseijelly@berkeley.edu>
2024-06-05 15:34:33 -0700

    cy config updates

--- soc/src/main/scala/RoSEConfigs.scala
+++ soc/src/main/scala/RoSEConfigs.scala
@@ -36,6 +36,26 @@ class RoseTLRocketStereoAccRoccConfig extends Config(
   new stereoacc.WithDefaultStereoAccConfig() ++
   new chipyard.config.AbstractRoseConfig)
 
+class RoseTLBOOMDualDMAConfig extends Config(
+  new rose.WithRoseAdapter(dst_ports = new DstParams_Container(Seq(
+    DstParams(port_type="DMA", DMA_address = 0x88000000L, name="DMA0"),
+    DstParams(port_type="DMA", DMA_address = 0x89000000L, name="DMA1"),
+    DstParams(port_type="reqrsp", name="reqrsp0"),
+  ))) ++     
+  new boom.common.WithNLargeBooms(1) ++ 
+  new chipyard.config.AbstractRoseConfig
+)
+
+class RoseTLRocketDualDMAConfig extends Config(
+  new rose.WithRoseAdapter(dst_ports = new DstParams_Container(Seq(
+    DstParams(port_type="DMA", DMA_address = 0x88000000L, name="DMA0"),
+    DstParams(port_type="DMA", DMA_address = 0x89000000L, name="DMA1"),
+    DstParams(port_type="reqrsp", name="reqrsp0"),
+  ))) ++     
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractRoseConfig
+)
+
 class RoseTLRocketStereoAccConfig extends Config(
   new rose.WithRoseAdapter(dst_ports = DstParams_Container(Seq(
     DstParams(port_type="DMA", DMA_address = 0x88000000L, name="DMA0"),
